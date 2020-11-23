c8657e524edf3f1fe5109b188de9c7d235aaddf2
John Brink <john-brink@hotmail.com>
2020-11-23 20:35:39 +0100

    all fixed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
-import './services/spotifyauth.js'
+import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a href="/login" class="btn btn-primary">Log in with Spotify</a>
-      <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [token, setToken] = useState();
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+
+    console.log("token", token);
+  }, [token]);
+
+  return <div className="app">{token ? <h1>Logged in {token}</h1> : <Login />}</div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { signup } from "./helpers/auth.js";
+//import { signup } from "./helpers/auth.js";
 
 ReactDOM.render(
   <React.StrictMode>
