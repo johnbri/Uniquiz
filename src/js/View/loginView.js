6757f72b4a2118af732dcf7bedf1fb9644d13464
emeurk <emeurk@kth.se>
2020-11-25 14:03:44 +0100

    Merge mellan Erik o Mickan

---
+++ src/js/View/loginView.js
@@ -0,0 +1,10 @@
+const LoginView= ({url}) =>
+    <div className="login">
+        <img
+            src="https://music-b26f.kxcdn.com/wp-content/uploads/2017/06/635963274692858859903160895_spotify-logo-horizontal-black.jpg"
+            alt="Spotify logo"
+        />
+        <a href={url}>LOGIN WITH SPOTIFY</a>
+    </div>;
+
+export {LoginView};
