1f30c080473a992d22e73824d6461135c170fd35
Jessie Liu <jessieljxliu@gmail.com>
2020-12-05 17:16:33 +0100

    Logout button for Home

--- src/js/View/homeView.js
+++ src/js/View/homeView.js
@@ -1,6 +1,6 @@
 import '../../css/Home.css';
 
-const homeView= ({onCreate, onJoin, userName}) =>
+const homeView= ({onCreate, onJoin, userName, onLogOut}) =>
     <div className="home">
         <img
         src = "logo.png"
@@ -9,6 +9,7 @@ const homeView= ({onCreate, onJoin, userName}) =>
         <h1>Welcome, {userName}!!</h1>
         <button onClick={() => onCreate()}>Create Quiz</button>
         <button onClick={() => onJoin()}>Join Quiz</button>
+        <button onClick={() => onLogOut()}>Log out</button>
     </div>;
 
 export {homeView};
