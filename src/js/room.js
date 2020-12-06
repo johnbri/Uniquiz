e40a867e5768e7937bd4752658d5a2929a33cc19
John Brink <john-brink@hotmail.com>
2020-12-06 16:31:50 +0100

    .

--- src/js/room.js
+++ src/js/room.js
@@ -1,16 +1,36 @@
 import React from "react";
 import { RoomView } from "./view/roomView";
+import CreateJoinRoom from "./createJoinRoom";
+import PromiseNoData from './view/promiseNoData.js';
 
 
 function Room(props){
 
-    return React.createElement(RoomView,{
-        roomName: "hej",
-        playerNames: "hej",
-        onExit: () => props.history.push("/home"),
-        onStart: () => props.history.push("/quiz")
-    });
+    return (React.Fragment, {}
+        , (RoomView,{
+            roomName: "hej",
+            playerNames: "hej",
+            onExit: () => props.history.push("/home"),
+            onStart: () => props.history.push("/quiz")
+            })/*
+            PromiseNoData(promise, data, error) ||
+                h(PlayersSidebarView,{
+                    hej: "hej"
+                    })*/          
+    )
 }
 
-
 export default Room;
+
+/*
+function Search({model, dishChoice}){
+    const [type, setType]= React.useState(""); 
+    const [query, setQuery]= React.useState("");
+
+    const [promise, setPromise]= React.useState(null);
+    React.useEffect(()=>setPromise(DishSource.searchDishes({})),
+    []);
+    
+    const [data, error]= usePromise(promise);
+
+    */
