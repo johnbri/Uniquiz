44fe403aae31e71d086f2cda52bb995da295e162
emeurk <emeurk@kth.se>
2020-12-07 15:53:15 +0100

    apa

--- src/js/readRoomModel.js
+++ src/js/readRoomModel.js
@@ -1,17 +1,15 @@
-import React, { useEffect } from 'react';
-import {auth, database, syncRoomModelToFB, updateRoomFB} from '../services/firebase.js';
+import {auth, database} from '../services/firebase.js';
 import RoomModel from './roomModel.js';
 import {roomModel, userModel} from "../index.js";
 
 
 function ReadRoomModel() {
-    /** Checks for data connected to currentRoom in firebase to put in the room model on refresh  */
     let model = new RoomModel();
     auth().onAuthStateChanged((userObject) => {
         if (userObject) {
             let roomName = userModel.currentRoom;
             if(roomName !== "") {
-                syncRoomModelToFB(model,roomName);
+                syncRoomsFB(model,roomName);
                 model.addObserver(()=> updateRoomFB(model, roomName));
             }
         } else {
@@ -24,17 +22,10 @@ function ReadRoomModel() {
 export {ReadRoomModel, createJoinRoomFB};
 
 
-<<<<<<< HEAD
 async function createJoinRoomFB(roomName, createRoom){
-=======
-function createJoinRoomFB(roomName, createRoom){
-    /** Check if given rooms exists in FB to create new or join room to model*/
-    let roomDataDB = {}; 
->>>>>>> 57e244c924db974c27c4df1e9476033a96e41c40
     database.ref('rooms/' + roomName).once('value', (snapshot) => {
         if (snapshot.val() !== null && createRoom) { //If room exist and user wants to create
                 console.log("A room with the name already exists");
-<<<<<<< HEAD
         } else if (snapshot.val() !== null && !createRoom) { //If room exist and user wants to join
             syncRoomsFB(roomModel,roomName);
             roomModel.setRoomName(roomName);
@@ -47,31 +38,10 @@ function createJoinRoomFB(roomName, createRoom){
             roomModel.addPlayers(userModel.uid);
             updateRoomFB(roomModel, roomName);
         } else { //If room does not exist and user wants to join
-=======
-            } else {
-                snapshot.forEach((child) => {
-                    roomDataDB[child.key]= child.val();
-                });
-                syncRoomModelToFB(roomModel,roomName);
-                roomModel.addObserver(()=> updateRoomFB(roomModel, roomName));
-                roomModel.setRoomName(roomName);
-                userModel.setCurrentRoom(roomName);
-                roomModel.addPlayers(userModel.uid);
-            }   
-        } else {
-            if(createRoom) {
-                syncRoomModelToFB(roomModel,roomName);
-                roomModel.addObserver(()=> updateRoomFB(roomModel, roomName));
-                roomModel.setRoomName(roomName);
-                userModel.setCurrentRoom(roomName);
-                roomModel.addPlayers(userModel.uid);
-            } else {
->>>>>>> 57e244c924db974c27c4df1e9476033a96e41c40
                 console.log("Room does not exist!");
         }
     });
 }
-<<<<<<< HEAD
 
 function updateRoomFB(model, roomName){
     database.ref('rooms/' + roomName).update({
@@ -92,5 +62,3 @@ function syncRoomsFB(model, roomName){
         console.log(error);
     }
 }
-=======
->>>>>>> 57e244c924db974c27c4df1e9476033a96e41c40
