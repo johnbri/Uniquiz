e42bd3c893b557d5c97420fa6504f561daaa6d4a
emeurk <emeurk@kth.se>
2020-12-11 18:01:48 +0100

    vettef

--- src/js/quizAnswers.js
+++ src/js/quizAnswers.js
@@ -31,28 +31,3 @@ function QuizAnswers (props) {
 }
 
 export default QuizAnswers;
-/*arrayuid.forEach(uid => {
-    database.ref('users/' + uid + '/token').once('value', (snapshot) => { 
-        getUser(snapshot.val())
-        .then((obj) => obj.items.forEach((track) => playlists.push([track.name, track.id])))
-        .then(console.log(playlists));             
-        });
-    });*/
-
-
-
-/* for (let i = 0; i < arrayuid.length; i++) {
-        let token = (await getUserToken(arrayuid[i])).val(); // hämtar ut token
-        let userPlaylist = (await getUserTopPlaylist(token)).items; // hämtar ut top 50 låtar från användaren
-
-        userPlaylist = userPlaylist.map((track) => [track.name, track.id]); // gör en ny array med bara namn och id
-        combinedPlaylist.push(userPlaylist); // lägger till i den stora playlisten med alla användares låtar
-    }
-*/
-
-/*useEffect(() => {
-    const timer = setTimeout(() => {
-    setTimeLeft(calculateTimeLeft());
-    }, 1000);
-    return () => clearTimeout(timer);
-});*/
