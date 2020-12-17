a7a8575571bab9a6c3d87f076a4cd3edc6d4e140
emeurk <emeurk@kth.se>
2020-12-17 13:49:51 +0100

    laddningsbaren går åt andra gållet nu

--- src/js/spotify.js
+++ src/js/spotify.js
@@ -59,7 +59,6 @@ export async function getUserPlaylists(token) {
     const playListsObj = await spotifyApiCall(token, apiObj.items[i].tracks.href) // får ned alla tracks
     playListsObj.items.forEach(trackObj => { // skulle kunna skriva om den här funktionen till en for loop så vi slipper errors...
         if (trackObj.track!=null && trackObj.track.id && trackObj.track.album.images[0]) {
-          console.log(trackObj.track)
           let artists = [];
           trackObj.track.artists.forEach(artist => artists.push(artist.name));
           allTracks.push([trackObj.track.name, artists, trackObj.track.id, trackObj.track.preview_url, trackObj.track.album.images[0].url])
